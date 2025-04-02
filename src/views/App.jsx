186f3e74b7d25d4f498dc8416ebcc05e062c4cb4
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-04-02 12:39:33 -0300

    feat: set up main application structure with routing and styling

---
+++ src/views/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import React from "react";
+
+import { BrowserRouter } from  'react-router-dom'
+
+import Menu from '../components/layout/Menu';
+import Content from '../components/layout/Content';
+
+const App = (props) => {
+    return (
+      <div className="App">
+   <BrowserRouter>
+
+        <Menu />
+        <Content />
+        
+    </BrowserRouter>    
+      </div>
+    );
+  };
+
+  export default App
