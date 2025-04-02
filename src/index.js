61604e520fa99434cf2f3edc3c84f434d65c46a7
Renato Cruz Souza <renato.souza.flooxmongagua@gmail.com>
2025-04-02 12:41:29 -0300

    fix: correct application entry point setup and styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import ReactDOM from 'react-dom/client'; 
+import './index.css'
+import ReactDOM from 'react-dom/client';
 
+
+import App from './views/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <>
- <h1>App</h1>
- </>
-);
+      <App />
+      
+    </>
+  );
 
